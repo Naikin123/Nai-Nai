8a326cb803a2ed5085ef610effc497321fea8d7b
Naikin123 <an3038489@gmail.com>
2026-01-18 23:06:42 -0600

    Update script.js

--- script.js
+++ script.js
@@ -1,3 +1,6 @@
+alert("¡EL CÓDIGO ESTÁ VIVO!");
+
+
 // --- 1. CONFIGURACIÓN ---
 const supabaseUrl = 'https://icxjeadofnotafxcpkhz.supabase.co';
 const supabaseKey = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImljeGplYWRvZm5vdGFmeGNwa2h6Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3NjgwOTM1MjEsImV4cCI6MjA4MzY2OTUyMX0.COAgUCOMa7la7EIg-fTo4eAvb-9lY83xemQNJGFnY7o'; 
