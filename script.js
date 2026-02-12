4a99e2eb657d6fd299e91eff6877c7321c0005b2
Naikin123 <an3038489@gmail.com>
2026-02-11 22:36:26 -0600

    Update script.js

--- script.js
+++ script.js
@@ -1,7 +1,3 @@
-const supabaseUrl = 'https://icxjeadofnotafxcpkhz.supabase.co';
-const supabaseKey = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImljeGplYWRvZm5vdGFmeGNwa2h6Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3NjgwOTM1MjEsImV4cCI6MjA4MzY2OTUyMX0.COAgUCOMa7la7EIg-fTo4eAvb-9lY83xemQNJGFnY7o';
-const supabase = createClient(https:https://icxjeadofnotafxcpkhz.supabase.co,);
-
 // Verifica si ya aceptó las reglas al cargar la página
 window.addEventListener('load', () => {
     if (localStorage.getItem('rulesAccepted') === 'true') {
